8f40f3cfe1187210045df7a1799be65ac465daee
BartIntec <bart.hunerbein@intecbrussel.be>
2021-04-23 10:52:50 +0200

    document the fetch function

--- datascript.js
+++ datascript.js
@@ -2,14 +2,20 @@ const url = "https://opendata.brussel.be/api/records/1.0/search/?dataset=comic-b
 let records;
 const main = document.querySelector('main');
 
+// use fetch api to get data from an url
+// after a while (by using the 'then' notation) we call upon a function that 
+// jsonifies the response
+// after this has been done, it can take a while, we call upon yet another function
+// this last function uses the json data and gives the commands that effectively make the content
+// appear on screen.
 fetch(url).then(function(response) {
     response.json().then(function(json) {
       records = json.records;
       console.log(json);
-      addContent(records[1].fields);
+      addContent(records[10].fields);
     });
   });
-
+console.log('other stuff');
  
 
 
